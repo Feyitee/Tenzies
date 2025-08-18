ee31b670d0c0c60c2ff9372f597bbce80fc17a8d
Feyitee <anjuwontobi99@gmail.com>
2025-08-18 15:12:25 +0100

    dice-in-state

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import React from "react";
 import Die from "./components/Die";
 
 const App = () => {
+  //function generateAllNewDice() {
+  // return Array.from({ length: 10 }, () => Math.floor(Math.random() * 6) + 1);
+
+  //return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+
+  function generateAllNewDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      const num = Math.ceil(Math.random() * 6);
+      dice.push(num);
+    }
+    return dice;
+  }
+
+  const [dice, setDice] = React.useState(generateAllNewDice());
+
+  console.log(generateAllNewDice());
+
+  const diceElements = dice.map((num) => {
+    return <Die value={num} />;
+  });
+
   return (
     <main className="dice">
-      <div className="dice-container">
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 };
