21ff214403dcb188c60a95b8c88cba1aa14c291f
Feyitee <anjuwontobi99@gmail.com>
2025-08-18 12:23:03 +0100

    dices

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Die from "./components/Die";
 
 const App = () => {
-  return <main>Hello</main>;
+  return (
+    <main className="dice">
+      <div className="dice-container">
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+        <Die value={1} />
+      </div>
+    </main>
+  );
 };
 
 export default App;
