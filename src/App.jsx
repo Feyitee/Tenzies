973498dba70a67c9c0335b352dba104e866d8523
Feyitee <anjuwontobi99@gmail.com>
2025-08-22 21:39:48 +0100

    holdrollfunctionsgenerateallnewdice

--- src/App.jsx
+++ src/App.jsx
@@ -26,15 +26,21 @@ const App = () => {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice());
+    setDice((prevDice) =>
+      prevDice.map((die) => {
+        return die.isHeld
+          ? die
+          : { ...die, value: Math.ceil(Math.random() * 6) };
+      })
+    );
   }
 
   function hold(id) {
-    setDice((oldDice) => {
-      return oldDice.map((die) => {
-        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-      });
-    });
+    setDice((oldDice) =>
+      oldDice.map((die) =>
+        die.id === id ? { ...die, isHeld: !die.isHeld } : die
+      )
+    );
   }
 
   const diceElements = dice.map((dieObj) => (
@@ -48,6 +54,11 @@ const App = () => {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
         Roll
